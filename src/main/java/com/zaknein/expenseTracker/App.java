8d50ae75006d64d466abba984e1f21567c0c691a
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-05-25 17:41:16 -0300

    Initial commit
    commit inicial

---
+++ src/main/java/com/zaknein/expenseTracker/App.java
@@ -0,0 +1,9 @@
+package com.zaknein.expenseTracker;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException{
+        System.out.println("hola");
+    }
+}
