ffdf77650738d72fe7160b643e2148328e545744
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-05-29 14:28:26 -0300

    add

--- src/main/java/com/zaknein/expenseTracker/App.java
+++ src/main/java/com/zaknein/expenseTracker/App.java
@@ -1,15 +1,41 @@
 package com.zaknein.expenseTracker;
 
 import java.io.IOException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class App {
+
+    private static final File expenseFile = new File("expense.json");
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+
+
     public static void main(String[] args) throws IOException {
-        System.out.println("hola");
+
 
         Expense expense = new Expense(10, "caca", 12);
     }
     
     public static void add(Expense expense){
+
+        var list = new ArrayList<Expense>();
+
+        if(expenseFile.exists()){
+            list = mapper.readValue(expenseFile, mapper.getTypeFactory().constructCollectionType(ArrayList.class, Expense.class));
+            list.add(Expense);        
+        }
+
+        try {
+            mapper.writeValue(expenseFile, lista);
+        } catch (IOException e) {
+            System.out.println("No existe archivo");
+        }
+        
 
     }
     public static void list(){
@@ -19,7 +45,7 @@ public class App {
 
     }
     public static void delete(){
-        
+
     }
 
 
