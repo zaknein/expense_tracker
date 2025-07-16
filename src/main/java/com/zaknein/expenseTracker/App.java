5564aed461672adab6b05f1cd49306530130acc0
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-07-16 09:50:33 -0300

    instanciar summary

--- src/main/java/com/zaknein/expenseTracker/App.java
+++ src/main/java/com/zaknein/expenseTracker/App.java
@@ -10,6 +10,7 @@ import com.zaknein.expenseTracker.comandos.AddExpense;
 import com.zaknein.expenseTracker.comandos.DeleteExpense;
 import com.zaknein.expenseTracker.comandos.ListExpenses;
 import com.zaknein.expenseTracker.comandos.UpdateExpense;
+import com.zaknein.expenseTracker.comandos.Summary;
 import com.zaknein.expenseTracker.logica.DeleteExpenseLogic;
 import com.zaknein.expenseTracker.logica.ListExpenseLogic;
 import com.zaknein.expenseTracker.logica.SaveExpenseLogic;
@@ -30,12 +31,14 @@ public class App {
         ListExpenses listExpenses = new ListExpenses();
         DeleteExpense deleteExpense = new DeleteExpense();
         UpdateExpense updateExpense = new UpdateExpense();
+        Summary summary = new Summary();
 
         // Instanciamos objetos de logica
         SaveExpenseLogic saveNewExpense = new SaveExpenseLogic(expenseStorage);
         DeleteExpenseLogic deleteExpenseLogic = new DeleteExpenseLogic(expenseStorage);
         ListExpenseLogic listExpenseLogic = new ListExpenseLogic(expenseStorage);
         UpdateExpenseLogic updateExpenseLogic = new UpdateExpenseLogic(expenseStorage);
+        SummaryLogic summaryLogic = new SummaryLogic(expenseStorage);
 
 
         JCommander jc = JCommander.newBuilder()
@@ -43,6 +46,7 @@ public class App {
                 .addCommand("list", listExpenses)
                 .addCommand("delete", deleteExpense)
                 .addCommand("update", updateExpense)
+                .addCommand("summary", summary)
                 .build();
 
         if (args.length == 0) {
@@ -60,6 +64,7 @@ public class App {
             case "list" -> listExpenseLogic.show();
             case "delete" -> deleteExpenseLogic.remove(deleteExpense.getId());
             case "update" -> updateExpenseLogic.update(updateExpense.getId(), updateExpense.getDescription(), updateExpense.getAmount());
+            case "summary" -> summaryLogic.summary();
 
         }
 
